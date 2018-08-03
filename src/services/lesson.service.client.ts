91e526ca4edecc5fdc4100ad9ec7cdb67f761e4a
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-03 14:18:07 -0400

    Created lesson service client

---
+++ src/services/lesson.service.client.ts
@@ -0,0 +1,10 @@
+export class LessonServiceClient {
+
+  findLessonsForModule(courseId, moduleId) {
+    return fetch
+    ('http://localhost:8080/api/course/'
+      + courseId + '/module/' + moduleId + '/lesson')
+      .then(response => response.json());
+  }
+
+}
