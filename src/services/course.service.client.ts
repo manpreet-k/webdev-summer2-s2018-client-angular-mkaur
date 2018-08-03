e6b8b568f98d62ef03ac55ab04d072bd4a06da1e
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-03 10:50:17 -0400

    Created Course grid client

---
+++ src/services/course.service.client.ts
@@ -0,0 +1,13 @@
+export class CourseServiceClient {
+
+  findAllCourses() {
+    return fetch
+    ('http://localhost:8080/api/course')
+      .then(response => response.json());
+  }
+
+  findCourseById(courseId) {
+    return fetch('http://localhost:8080/api/course/' + courseId)
+      .then(response => response.json());
+  }
+}
