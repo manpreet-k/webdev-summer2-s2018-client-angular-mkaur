af903572c31abbf40a032400310def39c39a88e4
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-03 13:44:01 -0400

    Created module service client

---
+++ src/services/module.service.client.ts
@@ -0,0 +1,10 @@
+export class ModuleServiceClient {
+
+  findModulesForCourse(courseId) {
+    return fetch
+    ('http://localhost:8080/api/course/'
+      + courseId + '/module')
+      .then(response => response.json());
+  }
+
+}
