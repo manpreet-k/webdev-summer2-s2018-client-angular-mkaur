efb90c9b2db6c60b5e3d6e292d7941088d61a683
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-05 02:43:34 -0400

    Added route to profile and home

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {CourseServiceClient} from '../../services/course.service.client';
+import {UserServiceClient} from '../../services/user.service.client';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -10,11 +12,20 @@ export class AdminComponent implements OnInit {
   courses = [];
   selectedCourseId = 0;
 
-  constructor(private service: CourseServiceClient) {
+  constructor(private service: CourseServiceClient,
+              private userService: UserServiceClient,
+              private router: Router) {
   }
 
   selectCourse(courseId) {
     this.selectedCourseId = courseId;
+  }
+
+  logout() {
+    this.userService
+      .logout()
+      .then(() =>
+        this.router.navigate(['login']));
   }
 
   ngOnInit() {
