ee3ca876d165c8d161afa25295fe14a51fb39abc
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-04 20:24:08 -0400

    Created profile component

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit} from '@angular/core';
+import {UserServiceClient} from '../../services/user.service.client';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor(private service: UserServiceClient,
+              private router: Router) {
+  }
+
+  user = {};
+  username;
+  password;
+  phone;
+  firstName;
+  lastName;
+  email;
+
+  update() {
+    const user = {
+      username: this.username,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      phone: this.phone,
+    };
+    this.service.update(user).then(() => {
+      alert(user);
+    });
+  }
+
+  ngOnInit() {
+    this.service
+      .profile()
+      .then(user => {
+        this.username = user.username;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+        this.email = user.email;
+        this.phone = user.phone;
+      });
+  }
+
+}
