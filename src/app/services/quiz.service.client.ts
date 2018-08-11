ad53d8a447d8afc7d8b1335177e7123f7501a7f8
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-11 16:08:09 -0400

    Added findQuizbyID and submit quiz methods

--- src/app/services/quiz.service.client.ts
+++ src/app/services/quiz.service.client.ts
@@ -4,8 +4,30 @@ import {Injectable} from '@angular/core';
 export class QuizServiceClient {
 
   URL = 'http://localhost:3000/api/quiz';
+
   findAllQuizzes() {
     return fetch(this.URL)
+      .then(response => response.json());
+  }
+
+  findQuizById(quizId) {
+    return fetch(this.URL + '/' + quizId)
+      .then(response => response.json());
+  }
+
+  submitQuiz(quiz, quizId, username) {
+    const quizSubmission = {
+      quizId: quizId,
+      username: username,
+      answers: quiz
+    };
+    return fetch(this.URL + '/' + quizId, {
+      method: 'post',
+      body: JSON.stringify(quizSubmission),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
       .then(response => response.json());
   }
 }
