6a5e99fff0826976027f59ce9d0c431ca7c5b95d
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-11 23:52:20 -0400

    Fixed URL

--- src/app/quiz-answers/quiz-answers.component.ts
+++ src/app/quiz-answers/quiz-answers.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {QuizServiceClient} from '../services/quiz.service.client';
+import {UserServiceClient} from '../services/user.service.client';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-quiz-answers',
@@ -7,7 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizAnswersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: QuizServiceClient,
+              private userService: UserServiceClient,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute
+      .params
+      .subscribe(params => this.loadQuizAnswers(params['quizId'], params['submissionId']));
+  }
+
+  quizId = '';
+  submissionId = '';
+  quiz = {};
+  submission = {};
+  answers = {};
+
+  loadQuizAnswers(quizId, submissionId) {
+    this.quizId = quizId;
+    this.submissionId = submissionId;
+
+    this.service
+      .findSubmissionById(this.submissionId)
+      .then(sub => {
+        this.submission = sub;
+        this.answers = sub.answers;
+        this.service
+          .findQuizById(quizId)
+          .then(quiz => this.quiz = quiz);
+      });
+  }
+
+  logout() {
+    this.userService
+      .logout()
+      .then(() =>
+        this.router.navigate(['login']));
+  }
 
   ngOnInit() {
   }
