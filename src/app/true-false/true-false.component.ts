f6118a5a2883da922001610389764fb857538c80
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-11 22:45:28 -0400

    Added readonly div

--- src/app/true-false/true-false.component.ts
+++ src/app/true-false/true-false.component.ts
@@ -9,6 +9,10 @@ export class TrueFalseComponent implements OnInit {
 
   @Input() question;
   @Input() submission;
+  @Input() makeReadOnly;
+  @Input() answer;
+
+  hide = false;
 
   constructor() { }
 
@@ -16,7 +20,28 @@ export class TrueFalseComponent implements OnInit {
     this.submission[this.question._id] = trueOrFalse;
   }
 
+  markAnswer(optionType) {
+    if (optionType) {
+      if (this.answer) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      if (this.answer) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
   ngOnInit() {
+    if (this.makeReadOnly) {
+      this.hide = false;
+    } else {
+      this.hide = true;
+    }
   }
 
 }
